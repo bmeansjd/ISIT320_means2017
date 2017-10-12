00aaed726a66fbc76e7b91cf9449c8a3ae50655c
Brenda Means <bjmeansjd@gmail.com>
2017-10-11 17:00:53 -0700

    week 1 and 4

---
+++ Week04-UseSiteTools/server/routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var isitSiteTools = require('isit-site-tools-calvertbc');
+/* GET home page. */
+router.get('/', function(req, res, next) { 'use strict';
+  res.render('index', { title: 'server' });
+});
+
+router.get('/foo', function(req, res, next) { 'use strict';
+    const foo = isitSiteTools.imageHelp.configSettings;
+    console.log(foo);
+    res.send({config: foo.getElvenImages()});
+});
+module.exports = router;
